b437517e779e04b911c52f8503b0370a0266c9fa
Sabeen Javaid <khannssabeen@gmail.com>
2024-06-12 16:15:03 +0200

    fetch art pieces

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  console.log(data);
   return (
     <div>
       <h1>Hello from Next.js</h1>
