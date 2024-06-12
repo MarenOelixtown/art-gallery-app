4cf2894dc2aa7f2e6bf9a30dff791ef67ea218a2
Sabeen Javaid <khannssabeen@gmail.com>
2024-06-12 17:00:47 +0200

    complete user story 1

--- components/ArtPiecePreview.js
+++ components/ArtPiecePreview.js
@@ -1,3 +1,11 @@
+import Image from "next/image";
+
 export default function ArtPiecePreview({ image, title, artist }) {
-  return <div></div>;
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{artist}</p>
+      <Image src={image} alt={title} height={144} width={144} />
+    </div>
+  );
 }
