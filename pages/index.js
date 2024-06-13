9144b6de40d7befd07ef01a856a2c5ee7b937e86
Sabeen Javaid <khannssabeen@gmail.com>
2024-06-13 09:31:37 +0200

    start user story 2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import ArtPieces from "@/components/ArtPieces";
+import { useEffect, useState } from "react";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -8,6 +9,20 @@ export default function HomePage() {
     "https://example-apis.vercel.app/api/art",
     fetcher
   );
+
+  const [randomPiece, setRandomPiece] = useState(null);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setRandomPiece(getRandomElement(data));
+    }
+  }, [data]);
+
+  console.log(randomPiece);
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
 
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
