4db3a9ea852321cf904fbeb5684109ba3cdb3bc6
Maren <marenehlers@gmx.net>
2024-06-13 10:09:19 +0200

    moving data fetching to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,36 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+import { useEffect, useState } from "react";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+  const [randomPiece, setRandomPiece] = useState(null);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setRandomPiece(getRandomElement(data));
+    }
+  }, [data]);
+
+  console.log(randomPiece);
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+  console.log(data);
+  console.log(data.name);
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component randomPiece={randomPiece} pieces={data} {...pageProps} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
-import useSWR from "swr";
 import ArtPieces from "@/components/ArtPieces";
-import { useEffect, useState } from "react";
 import Spotlight from "@/components/Spotlight";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-export default function HomePage() {
-  const { data, error, isLoading } = useSWR(
-    "https://example-apis.vercel.app/api/art",
-    fetcher
-  );
-
-  const [randomPiece, setRandomPiece] = useState(null);
-
-  useEffect(() => {
-    if (data && data.length > 0) {
-      setRandomPiece(getRandomElement(data));
-    }
-  }, [data]);
-
-  console.log(randomPiece);
-
-  function getRandomElement(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-
-  if (error) return <div>Failed to load</div>;
-  if (isLoading) return <div>Loading...</div>;
-  console.log(data);
-  console.log(data.name);
+export default function HomePage({ randomPiece, pieces }) {
   return (
     <div>
       {randomPiece && (
@@ -38,7 +11,7 @@ export default function HomePage() {
         />
       )}
 
-      <ArtPieces pieces={data} />
+      <ArtPieces pieces={pieces} />
     </div>
   );
 }
